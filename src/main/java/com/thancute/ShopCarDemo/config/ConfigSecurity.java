c7197e27e5a01f1443163132dbdcb88eac8bb6d6
Nguyen Quy Than <quythan306@gmail.com>
2024-02-01 16:11:41 +0700

    demo-logout

--- src/main/java/com/thancute/ShopCarDemo/config/ConfigSecurity.java
+++ src/main/java/com/thancute/ShopCarDemo/config/ConfigSecurity.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class ConfigSecurity {
@@ -23,7 +24,10 @@ public class ConfigSecurity {
             .anyRequest().authenticated())
             .formLogin(login -> login.loginPage("/login").permitAll()
             .usernameParameter("username").passwordParameter("passwords")
-            );
+            )
+            .logout(logout -> logout.invalidateHttpSession(true).clearAuthentication(true)
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/login?logout").permitAll());
 
         return httpSecurity.build();
     }
